04e06b389aa2070e248acc00a5cddabb6975a48b
Ajit, Patil(ajipatil) <ajipatil@lm-sjc-11002085.corp.ebay.com>
2016-09-28 18:43:33 -0700

    Add user form

---
+++ src/app/users/user-form.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import {User} from '../shared/models/user';
+
+@Component({
+  selector: 'user-form',
+  templateUrl: 'user-form.component.html',
+  styleUrls: ['user-form.component.css']
+})
+export class UserFormComponent implements OnInit {
+  constructor() {  }
+
+  ngOnInit() {}
+}
