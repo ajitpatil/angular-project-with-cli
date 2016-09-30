336220345eb7e1b131aefcb63b031bcffd29868b
Ajit, Patil(ajipatil) <ajit_patil@live.com>
2016-09-30 13:32:58 -0700

    remove user form and profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {UserProfileComponent} from './users/user-profile.component';
 import {SimpleChartExample} from './charts/highchart.component';
 import {ChartComponent} from 'angular2-highcharts';
 
@@ -12,7 +11,6 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
-    UserProfileComponent,
     SimpleChartExample,
     ChartComponent
   ],
