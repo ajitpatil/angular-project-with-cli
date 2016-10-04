48ba5f9b535cae537f29955f444d9b51db28152d
Ajit, Patil(ajipatil) <ajit_patil@live.com>
2016-10-04 13:57:03 -0700

    Add Master Detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {UserProfileComponent} from './users/user-profile.component';
+
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UserProfileComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
